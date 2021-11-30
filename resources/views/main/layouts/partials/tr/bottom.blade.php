a7c8c28d59864933da70a5e436a78ebca6c348ce
yura <yura.chernobay@gmail.com>
2021-11-30 21:18:45 +0200

    Small fixes

--- resources/views/main/layouts/partials/tr/bottom.blade.php
+++ resources/views/main/layouts/partials/tr/bottom.blade.php
@@ -1,12 +1,5 @@
-{{--<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 84">--}}
-{{--  <g>--}}
-{{--    <path transform="rotate(-82.01 155.661 313.508)" fill="#e23112" d="M-129.17-255.87h569.62V882.87h-569.62z"/>--}}
-{{--    <path fill="#e23112" opacity="0.64" d="M1143.71 6.17l590.79 166.28-1194.04-53.29L1143.71 6.17z"/>--}}
-{{--    <path fill="#e23112" opacity="0.64" d="M1517.56 37.45l574.63 165.56-1161.73-51.85 587.1-113.71z"/>--}}
-{{--  </g>--}}
-{{--</svg>--}}
-<section class="banner mb-20 -mt-20">
-    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewbox="0 25 160 40" class="waves"
+<section class="banner relative">
+    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewbox="0 25 160 40" class="waves absolute bottom-24 left-0"
          transform="matrix(1 0 0 -1 0 106)">
         <g>
             <use xlink:href="#wave" x="50" y="0" fill="#6a271b"/>
