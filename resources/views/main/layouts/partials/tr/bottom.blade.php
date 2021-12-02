1a2730bbd2e17a47abe0a012b270e81510929b7b
yura <yura.chernobay@gmail.com>
2021-12-02 13:21:13 +0200

    Fix waves animation

--- resources/views/main/layouts/partials/tr/bottom.blade.php
+++ resources/views/main/layouts/partials/tr/bottom.blade.php
@@ -1,18 +1,14 @@
-<section class="banner relative">
-    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewbox="0 25 160 40" class="waves absolute bottom-24 left-0"
-         transform="matrix(1 0 0 -1 0 106)">
-        <g>
-            <use xlink:href="#wave" x="50" y="0" fill="#6a271b"/>
-            <use xlink:href="#wave" x="40" y="4" fill="#e23112"/>
-            <use xlink:href="#wave" x="40" y="7" fill="#fff"/>
-
-        </g>
-        <defs>
-            <g transform="matrix(1 0 0 -1 0 106)">
-                <path id="wave"
-                      d="M-150 53c30.77 0 59.538-20 90-20 31.077 0 60.256 20 90 20 30.77 0 59.23-20 90-20 30.77 0 59.23 20 90 20v20h-360z"/>
-            </g>
+<div class="banner">
+    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewbox="0 25 160 40" class="h-12 xl:h-24 object-fill">
+         <defs>
+                <path id="wave2"
+                      d="M-150 47c30.77 0 59.538 20 90 20 31.077 0 60.256-20 90-20 30.77 0 59.23 20 90 20 30.77 0 59.23-20 90-20V27h-360z"/>
         </defs>
+        <g>
+            <use xlink:href="#wave2" x="50" y="0" fill="#6a271b"/>
+            <use xlink:href="#wave2" x="50" y="-2" fill="#e23112"/>
+            <use xlink:href="#wave2" x="50" y="-4" fill="#ffffff"/>
+        </g>
     </svg>
-</section>
+</div>
 
